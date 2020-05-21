2eee8f7dfdf03d023641dbd712253602ab1ed7e8
seffeng <seffeng@sina.cn>
2020-05-22 00:14:55 +0800

    update.

--- src/Rules/Password.php
+++ src/Rules/Password.php
@@ -1,57 +1,16 @@
 <?php
 /**
  * @link http://github.com/seffeng/
- * @copyright Copyright (c) 2019 seffeng
+ * @copyright Copyright (c) 2020 seffeng
  */
 namespace Seffeng\Basics\Rules;
-
-use Illuminate\Contracts\Validation\Rule;
 
 /**
  * 密码验证
  * @author zxf
- * @date   2020年4月30日
+ * @date   2020年5月21日
  */
-class Password implements Rule
+class Password extends \Seffeng\LaravelRules\Rules\Password
 {
-    /**
-     * 必须包含字母和数字
-     * @var string
-     */
-    protected $regex = '/^(?![0-9]+$)(?![a-zA-Z]+$)[0-9A-Za-z]{6,20}$/';
 
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct(string $regex = null)
-    {
-        if (!is_null($regex)) {
-            $this->regex = $regex;
-        }
-    }
-
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
-    public function passes($attribute, $value)
-    {
-        //
-        return boolval(preg_match($this->regex, $value));
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return ':attribute格式错误！';
-    }
 }
--- src/Rules/Phone.php
+++ src/Rules/Phone.php
@@ -1,60 +1,16 @@
 <?php
 /**
  * @link http://github.com/seffeng/
- * @copyright Copyright (c) 2019 seffeng
+ * @copyright Copyright (c) 2020 seffeng
  */
 namespace Seffeng\Basics\Rules;
-
-use Illuminate\Contracts\Validation\Rule;
 
 /**
  * 手机号验证
  * @author zxf
- * @date    2019年11月15日
+ * @date    2020年05月21日
  */
-class Phone implements Rule
+class Phone extends \Seffeng\LaravelRules\Rules\Phone
 {
-    /**
-     *
-     * @var string
-     */
-    protected $regex = '/^1\d{10}$/';
 
-    /**
-     * Create a new rule instance.
-     *
-     * @return void
-     */
-    public function __construct(string $regex = null)
-    {
-        if (!is_null($regex)) {
-            $this->regex = $regex;
-        }
-    }
-
-    /**
-     * Determine if the validation rule passes.
-     *
-     * @param  string  $attribute
-     * @param  mixed  $value
-     * @return bool
-     */
-    public function passes($attribute, $value)
-    {
-        //
-        if (is_numeric($value)) {
-            return boolval(preg_match($this->regex, $value));
-        }
-        return false;
-    }
-
-    /**
-     * Get the validation error message.
-     *
-     * @return string
-     */
-    public function message()
-    {
-        return ':attribute格式错误！';
-    }
 }
