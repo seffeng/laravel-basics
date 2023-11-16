b6801b868eadb8d682ba4c49e9548bb32ae43e1b
seffeng <seffeng@sina.cn>
2023-11-16 17:28:15 +0800

    update.

--- src/Exceptions/BaseException.php
+++ src/Exceptions/BaseException.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @link http://github.com/seffeng/
- * @copyright Copyright (c) 2019 seffeng
+ * @copyright Copyright (c) 2023 seffeng
  */
 namespace Seffeng\Basics\Exceptions;
 
@@ -9,5 +9,28 @@ use Symfony\Component\Routing\Exception\ExceptionInterface;
 
 class BaseException extends \RuntimeException implements ExceptionInterface
 {
+    /**
+     *
+     * @author zxf
+     * @date   2023-11-16
+     * @param string $message
+     * @param integer $code
+     * @param \Throwable|null $previous
+     */
+    public function __construct(string $message = '', int $code = 0, \Throwable $previous = null)
+    {
+        $message === '' && $message = $this->defaultMessage();
+        parent::__construct($message, $code, $previous);
+    }
 
+    /**
+     *
+     * @author zxf
+     * @date   2023-11-16
+     * @return string
+     */
+    protected function defaultMessage()
+    {
+        return '';
+    }
 }
